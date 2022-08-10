42d04a596e8e990ed621969dc8c4a52d825f9ade
VankataNikolov <73016993+VankataNikolov@users.noreply.github.com>
2022-08-10 20:38:20 +0300

    add validation for confirm password

--- src/components/users/Register/Register.js
+++ src/components/users/Register/Register.js
@@ -17,7 +17,7 @@ function Register() {
     const onFinish = async (values) => {
         const { username, password } = values;
 
-        try{
+        try {
             const response = await authService.register(username, password);
             const user = await response.json();
             if (user.message) {
@@ -26,9 +26,9 @@ function Register() {
                 return;
             }
             userLogin(user);
-    
+
             navigate("/", { replace: true });
-        }catch(err){
+        } catch (err) {
             console.log(err);
         }
 
@@ -40,81 +40,91 @@ function Register() {
 
     return (
         <>
-        <FormHeader title="Register" errorMessage={error}/>
-        <Form
-            name="register"
-            labelCol={{
-                span: 8,
-            }}
-            wrapperCol={{
-                span: 10,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <Form.Item
-                label="Username"
-                name="username"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your username!',
-                    },
-                    {
-                        min: 4,
-                        message: 'Username must be 4 characters at least'
-                    }
-                ]}
-            >
-                <Input />
-            </Form.Item>
-
-            <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                    {
-                        min: 6,
-                        message: 'Password must be 6 characters at least'
-                    }
-                ]}
-            >
-                <Input.Password />
-            </Form.Item>
-
-            <Form.Item
-                label="Confirm password"
-                name="confirm password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                    {
-                        min: 6,
-                        message: 'Password must be 6 characters at least'
-                    }
-                ]}
-            >
-                <Input.Password />
-            </Form.Item>
-
-            <Form.Item
-                wrapperCol={{
-                    offset: 8,
-                    span: 16,
+            <FormHeader title="Register" errorMessage={error} />
+            <Form
+                name="register"
+                labelCol={{
+                    span: 8,
                 }}
+                wrapperCol={{
+                    span: 10,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
             >
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                        {
+                            min: 4,
+                            message: 'Username must be 4 characters at least'
+                        }
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                        {
+                            min: 6,
+                            message: 'Password must be 6 characters at least'
+                        }
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    label="Confirm password"
+                    name="confirm password"
+                    dependencies={['password']}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                        {
+                            min: 6,
+                            message: 'Password must be 6 characters at least'
+                        },
+                        ({ getFieldValue }) => ({
+                            validator(_, value) {
+                                if (!value || getFieldValue('password') === value) {
+                                    return Promise.resolve();
+                                }
+
+                                return Promise.reject(new Error('Password missmatch !'));
+                            },
+                        })
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
         </>
     );
 }
